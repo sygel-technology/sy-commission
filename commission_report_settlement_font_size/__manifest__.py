55ff42ca9b9f536050fc1b93cb95de3ccec4fdfd
sygel <alberto.martinez@sygel.es>
2024-04-18 11:27:56 +0200

    [T-5709][ADD]commission_report_settlement_font_size,commission_settlement_line_percentage and commission_report_settlement_grouped modules

---
+++ commission_report_settlement_font_size/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2024 Alberto Martínez <alberto.martinez@sygel.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': 'Commission Report Settlement Font Size',
+    'summary': 'Change the report settlement font size to 12px.',
+    'version': '15.0.1.0.0',
+    'category': 'Commissions',
+    'website': 'https://sygel.es',
+    'author': 'Sygel',
+    'license': 'AGPL-3',
+    'application': False,
+    'installable': True,
+    'depends': [
+        'commission',
+    ],
+    'data': [
+        'report/report_settlement_document.xml',
+    ],
+}
